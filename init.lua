eea516283c6b7a5787f7545b2c450d78f934fcbd
Abigail Read <AbbyReadMail@gmail.com>
2026-02-15 17:16:48 -0800

    Parse the other setting too

--- init.lua
+++ init.lua
@@ -2,8 +2,12 @@
 
 local _upward_rejump = core.settings:get_bool("upward_rejump.use_old_code")
 if _upward_rejump == nil then
-	-- Default to the new block jump physics code
 	_upward_rejump = false
+end
+
+local _loose_lips = core.settings:get_bool("loose_lips.use_old_code")
+if _loose_lips == nil then
+	_loose_lips = false
 end
 
 core.register_on_joinplayer(function(player)
